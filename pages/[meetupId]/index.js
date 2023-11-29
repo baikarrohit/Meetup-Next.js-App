c3e3478b3c7fecf02674e16f18e920cdc12dc939
baikarrohit <baikarrohit1999@gmail.com>
2023-11-29 14:26:27 +0530

    Prerendering the powers of Next JS

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,45 @@ const MeetupDetailsPage = () => {
   );
 };
 
+export const getStaticPaths = async () => {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+      {
+        params: {
+          meetupId: "m3",
+        },
+      },
+    ],
+  };
+};
+
+export const getStaticProps = async (context) => {
+  //fetch data foe a single meetup
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+        id: meetupId,
+        title: "A first meetup.",
+        address: "KT2/108, ghatkopar, mumbai-400023, India",
+        description: "This is a first meetup.",
+      },
+    },
+  };
+};
 export default MeetupDetailsPage;
--- pages/index.js
+++ pages/index.js
@@ -17,10 +17,38 @@ const DUMMY_MEETUPS = [
     address: "KT6/306, mulund, mumbai-400023, India",
     description: "This is a second meetup.",
   },
+  {
+    id: "m3",
+    title: "A third meetup.",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "KT6/306, mulund, mumbai-400023, India",
+    description: "This is a third meetup.",
+  },
 ];
 
-const HomePage = () => {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+const HomePage = (props) => {
+  return <MeetupList meetups={props.meetups} />;
 };
 
+// export const getServerSideProps = (context) => {
+//   const req = context.req;
+//   const res = context.res;
+  
+//   //fetch data from an API
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// };
+
+export const getStaticProps = async () => {
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS
+    },
+    revalidate: 1
+  }
+};
 export default HomePage;
